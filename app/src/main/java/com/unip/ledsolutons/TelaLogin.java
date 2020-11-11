3df8642660cc194fce41ccbcb57d635d21c94ab9
Everlyn <yamamotoeverlyn@gmail.com>
2020-11-11 20:12:26 -0300

    criei tela de login

---
+++ app/src/main/java/com/unip/ledsolutons/TelaLogin.java
@@ -0,0 +1,35 @@
+
+
+package com.unip.ledsolutons;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_login);
+        Button btLogin = (Button) findViewById(R.id.btLogin);
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                TextView tEmail =(TextView) findViewById(R.id.tEmail);
+                TextView tSenha = (TextView) findViewById(R.id.tSenha);
+                String login = tEmail.getText().toString();
+                String senha = tSenha.getText().toString();
+                if(login.equals("Everlyn") && senha.equals("123")){
+                    alert("Logado com sucesso!");
+
+                }else{
+                    alert("Email e/ou senha incorreto(s)");
+                }
+            }
+        });
+
+    }
+    private void alert(String s) {
+        Toast.makeText(this,s,Toast.LENGTH_LONG).show();
+    }
+}
